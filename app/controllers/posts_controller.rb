92c003c105c33dc59dc5cf20073000bc1281d4ab
生方貴大 <>
2025-12-29 21:13:07 +0900

    createメソッド作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,25 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @posts = Post.all
   end
 
   def new
     @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, notice: 'Posted'
+    else
+      flash.now[:error] = 'Post failured'
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content, images: [])
   end
 end
