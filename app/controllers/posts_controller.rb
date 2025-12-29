d3735d8b464e5bc777edd5d8af1791cfd6bdf184
生方貴大 <>
2025-12-29 13:30:34 +0900

    new post formの側作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
     @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
   end
 end
