ad1162b4de74e32f665bc0ffb93bd2df64ad1855
生方貴大 <>
2025-12-29 21:24:05 +0900

    画像ポスト機能作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   def index
-    @posts = Post.all
+    @posts = Post.all.order('created_at DESC')
   end
 
   def new
