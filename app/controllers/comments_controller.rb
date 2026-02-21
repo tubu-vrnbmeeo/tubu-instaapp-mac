9f74830d3c3e223e4232e9536f6c8618ebbe93e2
生方貴大 <>
2026-02-21 23:34:04 +0900

    comment画面作成、フォーム作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,16 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only: [:create]
+  def index
+    post = Post.find(params[:post_id])
+    @comments = post.comments
+  end
+
+  def create
+
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
