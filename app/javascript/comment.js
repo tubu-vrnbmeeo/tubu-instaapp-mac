1ba8495e986dd2a78fbf6e1fde1907de1c0dbf67
生方貴大 <>
2026-03-22 20:12:37 +0900

    commentフォームを下に固定。改行数によってフォームを広げるようにした。

---
+++ app/javascript/comment.js
@@ -0,0 +1,10 @@
+import $ from 'jquery';
+
+$(function() {
+  var $textarea = $('#textarea');
+  var lineHeight = parseInt($textarea.css('lineHeight'));
+  $textarea.on('input', function(e) {
+    var lines = ($(this).val() + '\n').match(/\n/g).length;
+    $(this).height(lineHeight * lines);
+  });
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,6 +3,7 @@
 pin 'application'
 pin 'profile'
 pin 'post'
+pin 'comment'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
